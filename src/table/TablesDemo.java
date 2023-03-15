16ae627445b1aaa06a36890e50f9a994392b4e1b
khalil <khalillakhdharatc@gmail.com>
2023-03-15 11:44:01 +0100

    tables

---
+++ src/table/TablesDemo.java
@@ -0,0 +1,21 @@
+package table;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TablesDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List<Integer> maListe=new ArrayList<>();
+maListe.add(1);
+maListe.add(3);
+maListe.add(6);
+//méthode 1
+for(int ls : maListe)
+{
+System.out.println(ls);	
+}
+}
+
+}
